a041be139421b3fe5279daa1d5c30b5da742e8d5
AynanshAadyant <aynaadyant@gmail.com>
2026-05-07 20:42:27 +0530

    profile ready

---
+++ src/schemas/profile.schema.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const profileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    phoneNo: {
+        type: Number,
+        required: [true, "Phone number is required"],
+        unique: [true, "Phone number already exist"],
+        trim: true
+    },
+    location: {
+        type: String,
+        required: [true, "Location is required"]
+    },
+    linkedIn: {
+        type: String,
+        trim: true
+    },
+    github: {
+        type: String,
+        trim: true
+    },
+    portfolio: {
+        type: String,
+        trim: true
+    }
+}, { timestamps: true })
+
+const Profile = mongoose.model("Profile", profileSchema);
+export default Profile;
