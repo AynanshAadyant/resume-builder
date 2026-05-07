ba865fba8a72bbe584be8061e36038e3e0e4ecb1
AynanshAadyant <aynaadyant@gmail.com>
2026-05-07 20:24:09 +0530

    Used the correct middleware in routes

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,13 +1,13 @@
 import Profile from "../controllers/profile.controller.js";
 import { Router } from "express";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/create", authMiddleware, Profile.create);
-router.get("/get", authMiddleware, Profile.get);
+router.post("/create", isAuthenticated, Profile.create);
+router.get("/get", isAuthenticated, Profile.get);
 router.get("/getAll", Profile.getAll);
-router.put("/update", authMiddleware, Profile.update);
-router.delete("/delete", authMiddleware, Profile.delete);
+router.put("/update", isAuthenticated, Profile.update);
+router.delete("/delete", isAuthenticated, Profile.delete);
 
 export default router;
